325674c00260e67ff9ce788ac63ef0edba70c9a1
maciek666 <czerwoneszelki19@gmail.com>
2020-03-18 01:35:46 +0100

    Jednowarstowy

--- OneLayerNet.py
+++ OneLayerNet.py
@@ -2,8 +2,10 @@ import numpy as np
 import random as rand
 import math
 
+import OneLayerNet
 
-class OneLayeNet:
+
+class OneLayerNet:
 
     # S - wejscie
     # K - wyjscia
@@ -19,7 +21,7 @@ class OneLayeNet:
     # U - pobudzenie neuronu
     @staticmethod
     def dzialaj1(W, X):
-        B = 2
+        B = 5
         WT = W.transpose()
         U = WT.dot(X)
         return 1 / (1 + np.exp(-U * B))
@@ -32,32 +34,36 @@ class OneLayeNet:
         for i in range(epoki):
             x = rand.randint(0, kolumny - 1)
             X = Pyrzklady[:, x]
-            Y = OneLayeNet.dzialaj1(Wpo, X)
+            Y = OneLayerNet.dzialaj1(Wpo, X)
             D = (Testy[:, x] - Y.transpose()).transpose()
             dW = wsp_uczenia * X * D.transpose()
             Wpo = Wpo + dW
         return Wpo
 
 
-W = OneLayeNet.init1(5, 3)
-X = OneLayeNet.init1(5, 1)
-P = np.matrix('4.0 0.01 0.01 1 1.5; 2.0 1.0 2.0 2.5 2.0; 1 3.5 0.1 2.0 1.5').transpose()
-T = np.identity(3)
-print('zbior uczacy')
-print(P)
+def main():
+    W = OneLayerNet.init1(5, 3)
+    X = OneLayerNet.init1(5, 1)
+    P = np.matrix('4.0 0.01 0.01 0.02 -0.1; 2.0 1.0 2.0 2.5 2.0; 1 3.5 0.1 2.0 1.5').transpose()
+    T = np.identity(3)
+    print('zbior uczacy')
+    print(P)
 
-Wpo = OneLayeNet.ucz1(W, P, T, 100)
+    Wpo = OneLayerNet.ucz1(W, P, T, 100)
+
+    print('\n\nPO UCZENIU\n')
+    odp = OneLayerNet.dzialaj1(Wpo, P[:, 1])
+    print(odp)
+    czlowieki = np. \
+        matrix('2.0 0.0 0.0 0.0 0.0; 2.0 0.2 0.01 0.5 0.0; 2 0.5 0.1 0.0 0.5'). \
+        transpose()
+
+    czlowiek = OneLayerNet.dzialaj1(Wpo, czlowieki[:, 2])
+    print('\n\nCZLOWIEK\n')
+    print(czlowieki)
+    print('\n')
+    print(czlowiek)
 
 
-print('\n\nPO UCZENIU\n')
-odp = OneLayeNet.dzialaj1(Wpo, P[:, 1])
-print(odp)
-czlowieki = np.\
-    matrix('2.0 0.0 0.0 0.0 0.0; 2.0 0.2 0.01 0.5 0.0; 2 0.5 0.1 0.0 0.5')\
-    .transpose()
-
-czlowiek = OneLayeNet.dzialaj1(Wpo, czlowieki[:, 2])
-print('\n\nCZLOWIEK\n')
-print(czlowieki)
-print('\n')
-print(czlowiek)
+if __name__ == '__main__':
+    main()
